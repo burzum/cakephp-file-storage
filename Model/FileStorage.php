114cf478b1e70e62f7cdc171c62f561aa8c77fb9
Florian Krämer <florian.kraemer@cakedc.com>
2012-04-07 18:55:46 +0200

    Updating readme.md and FileStorage.php

--- Model/FileStorage.php
+++ Model/FileStorage.php
@@ -1,4 +1,6 @@
 <?php
+App::uses('Folder', 'Utility');
+App::uses('FileStorageAppModel', 'FileStorage.Model');
 /**
  * FileStorage
  *
@@ -6,7 +8,7 @@
  * @copyright 2012 Florian Krämer
  * @license MIT
  */
-class FileStorage extends AppModel {
+class FileStorage extends FileStorageAppModel {
 /**
  * Name
  *
@@ -35,9 +37,14 @@ class FileStorage extends AppModel {
  */
 	public $adapters = array(
 		'Local' => array(
-			'adapterOptions' => array(),
+			'adapterOptions' => array(TMP, true),
 			'adapterClass' => '\Gaufrette\Adapter\Local',
 			'class' => '\Gaufrette\Filesystem'));
+
+/**
+ * 
+ */
+	public $activeAdapter = 'Local';
 
 /**
  * Constructor
@@ -49,10 +56,26 @@ class FileStorage extends AppModel {
  */
 	public function __construct($id = false, $table = null, $ds = null) {
 		parent::__construct($id, $table, $ds);
-		$adapterConfig = Configure::read('FileStorage.adapter');
+		$adapterConfig = Configure::read('FileStorage.adapters');
 		if (is_array($adapterConfig)) {
-			$this->adapters = Set::merge($this->adapters, $adapterConfig);
+			$this->adapters = array_merge($this->adapters, $adapterConfig);
 		}
+	}
+
+/**
+ * Sets or gets the active storage adapter
+ *
+ * @param string
+ * @return mixed
+ */
+	public function adapter($adapter = null) {
+		if (empty($adapter)) {
+			return $this->activeAdapter;
+		}
+		if (isset($this->adapters[$adapter])) {
+			return $this->activeAdapter = $adapter;
+		}
+		return  false;
 	}
 
 /**
@@ -67,12 +90,63 @@ class FileStorage extends AppModel {
 	}
 
 /**
+ * beforeSave callback
+ *
+ * @return boolean true on success
+ */
+	public function beforeSave($options) {
+		if (!empty($this->data[$this->alias]['file']['tmp_name'])) {
+			$File = new File($this->data[$this->alias]['file']['tmp_name']);
+			$this->data[$this->alias]['filesize'] = $File->size();
+			$this->data[$this->alias]['mime_type'] = $File->mime();
+		}
+		if (!empty($this->data[$this->alias]['file']['name'])) {
+			$this->data[$this->alias]['extension'] = $this->fileExtension($this->data[$this->alias]['file']['name']);
+		}
+		if (!empty($this->data[$this->alias]['file']['name'])) {
+			$this->data[$this->alias]['filename'] = $this->data[$this->alias]['file']['name'];
+		}
+		$this->data[$this->alias]['adapter'] = $this->activeAdapter;
+		return true;
+	}
+
+/**
+ * Get a copy of the actual record before we delete it to have it present in afterDelete
+ *
+ * @return boolean
+ */
+	public function beforeDelete() {
+		$this->record = $this->find('first', array(
+			'contain' => array(),
+			'conditions' => array(
+				$this->alias . '.' . $this->primaryKey => $this->id)));
+		if (empty($this->record)) {
+			return false;
+		}
+		return true;
+	}
+
+/**
+ * @todo error handling, catch exceptions from the adapters
+ */
+	public function afterDelete() {
+		$Storage = $this->storageAdapter($this->record[$this->alias]['adapter']);
+		$Storage->delete($this->record[$this->alias]['path']);
+	}
+
+/**
  * StorageAdapter
  *
- * @param mixed $adapterName string or array
+ * @param mixed $adapterName string of adapter configuration or array of settings
  * @param boolean $renewObject
+ * @return Gaufrette object as configured by first arg
  */
-	public function storageAdapter($adapterName, $renewObject = false) {
+	public function storageAdapter($adapterName = null, $renewObject = false) {
+		if (empty($adapterName)) {
+			$adapterName = $this->activeAdapter;
+		}
+
+		$isConfigured = true;
 		if (is_string($adapterName)) {
 			if (!empty($this->adapters[$adapterName])) {
 				$adapter = $this->adapters[$adapterName];
@@ -85,12 +159,55 @@ class FileStorage extends AppModel {
 			}
 		}
 
+		if (is_array($adapterName)) {
+			$adapter = $adapterName;
+			$isConfigured = false;
+		}
+
 		$class = $adapter['adapterClass'];
 		$rc = new ReflectionClass($class);
 		$adapterObject = $rc->newInstanceArgs($adapter['adapterOptions']);
 		$engineObject = new $adapter['class']($adapterObject);
-		$this->adapters[$adapterName]['object'] = $engineObject;
-		return $this->adapters[$adapterName]['object'];
+		if ($isConfigured) {
+			$this->adapters[$adapterName]['object'] = &$engineObject;
+		}
+		return $engineObject;
+	}
+
+/**
+ * Creates a tmp file name and checks the tmp path, creates one if required
+ *
+ * This method is thought to be used to generate tmp file locations for use cases
+ * like audio or image process were you need copies of a file and want to avoid
+ * conflicts. By default the tmp file is generated using cakes TMP constant + 
+ * folder if passed and a uuid as filename.
+ *
+ * @param string $folder
+ * @param boolean $checkAndCreatePath
+ * @return string For example /var/www/app/tmp/<uuid> or /var/www/app/tmp/<my-folder>/<uuid>
+ */
+	public function tmpFile($folder = null, $checkAndCreatePath = true) {
+		if (empty($folder)) {
+			$path = TMP;
+		} else {
+			$path = TMP . $folder . DS;
+		}
+
+		if ($checkAndCreatePath === true && !is_dir($path)) {
+			new Folder($path, true);
+		}
+
+		return $path . String::uuid();;
+	}
+
+/**
+ * Removes the - from the uuid
+ *
+ * @param string uuid with -
+ * @return string uuid without -
+ */
+	public function stripUuid($uuid) {
+		return str_replace('-', '', $uuid);
 	}
 
 }
--- readme.md
+++ readme.md
@@ -1,6 +1,8 @@
 # FileStorage plugin for CakePHP 2.x
 
-This is work in progress!
+This is work in progress, the code might and very likely will change for some more time.
+
+So please do not ask yet or create issue tickets.
 
 ## Installation
 
@@ -11,6 +13,64 @@ To be able to simple autoload Gaufrette load the plugin with bootstrap enabled. 
 This plugin depends on the Gaufrette library (https://github.com/KnpLabs/Gaufrette), init the submodule, the plugin depends on it.
 
 	git submodule update --init
+
+If you want to use S3 upload Gaufrette has also submodules to initialize. Here is the whole story to get everything initialized:
+
+	git clone git://github.com/burzum/FileStorage.git
+	cd FileStorage
+	git submodule update --init
+	cd Vendor/Gaufrette
+	git submodule update --init
+
+## Usage
+
+You can configure as many file storage adapters as you want with different settings via Configure:
+
+	Configure::write('FileStorage.adapters', array(
+		'Local' => array(
+			'adapterOptions' => array(TMP, true),
+			'adapterClass' => '\Gaufrette\Adapter\Local',
+			'class' => '\Gaufrette\Filesystem')));
+
+To invoke a new instace using a configuration call:
+
+	$FileStorage->storageAdapter('Local');
+
+Alternativly you can pass a config array as first argument.
+
+### Image Versioning
+
+You can set up automatic image processing for the FileStorage.Image model.
+
+All you need to do is basically use the image model and configure versions on a per model basis. When you save an Image model record it is important to have the 'model' field filled so that the script can find the correct versions for that model.
+
+	Configure::write('Media', array(
+		'imageSizes' => array(
+			'GalleryImage' => array(
+				'c50' => array(
+					'crop' => array(
+						'width' => 50, 'height' => 50)),
+				't120' => array(
+					'thumbnail' => array(
+						'width' => 120, 'height' => 120))),
+				't800' => array(
+					'thumbnail' => array(
+						'width' => 800, 'height' => 600))),
+			'User' => array(
+				'c50' => array(
+					'crop' => array(
+						'width' => 50, 'height' => 50)),
+				't150' => array(
+					'crop' => array(
+						'width' => 150, 'height' => 150))),
+			)
+		)
+	);
+	ClassRegistry::init('FileStorage.Image')->generateHashes();
+
+Calling generateHashes is important, it will create the hash values for each versioned image.
+
+If you do not want to have the script generated the hashes each time its execute it is up to you to store it persistant. This plugin just provides you the tools.
 
 ## Requirements
 
