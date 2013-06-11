1932bf1536a9c4181ca0427a6f912cc4bb163db9
Florian Krämer <florian.kraemer@cakedc.com>
2013-06-11 02:15:29 +0200

    Adding tests

--- Test/Case/Model/FileStorageTest.php
+++ Test/Case/Model/FileStorageTest.php
@@ -14,8 +14,8 @@ class FileStorageTest extends CakeTestCase {
  *
  * @return void
  */
-	public function startTest($method) {
-		parent::startTest($method);
+	public function setUp() {
+		parent::setUp();
 		$this->FileStorage = new FileStorage();
 	}
 
@@ -37,6 +37,29 @@ class FileStorageTest extends CakeTestCase {
  */
 	public function testObject() {
 		$this->assertTrue(is_a($this->FileStorage, 'FileStorage'));
+	}
+
+/**
+ * testFsPath
+ *
+ * @return void
+ */
+	public function testFsPath() {
+		$result = $this->FileStorage->fsPath('Foobar', 'random-id');
+		$this->assertEqual($result, 'Foobar\63\87\12\randomid\\');
+
+		$result = $this->FileStorage->fsPath('Foobar', 'random-id', false);
+		$this->assertEqual($result, 'Foobar\63\87\12\\');
+	}
+
+/**
+ * testStripUuid
+ *
+ * @return void
+ */
+	public function testStripUuid() {
+		$result = $this->FileStorage->stripUuid('some-string-with-dashes');
+		$this->assertEqual($result, 'somestringwithdashes');
 	}
 
 }
