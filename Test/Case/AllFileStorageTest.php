ba510438423a2157ae5c781f9d6ebf070352aad7
Florian Krämer <florian.kraemer@cakedc.com>
2013-06-21 11:47:59 +0200

    Removing redundant $path declaration from AllFileStorageTest.php

--- Test/Case/AllFileStorageTest.php
+++ Test/Case/AllFileStorageTest.php
@@ -4,27 +4,24 @@
  */
 class AllFileStorageTest extends PHPUnit_Framework_TestSuite {
 
-	/**
-	 * Suite method, defines tests for this suite.
-	 *
-	 * @return void
-	 */
+/**
+ * Suite method, defines tests for this suite.
+ *
+ * @return void
+ */
 	public static function suite() {
 		$Suite = new CakeTestSuite('All FileStorage tests');
 
 		$path = dirname(__FILE__);
+
 		$Suite->addTestDirectory($path . DS . 'Lib');
 
-		$path = dirname(__FILE__);
 		$Suite->addTestDirectory($path . DS . 'Lib' . DS . 'Utility');
 
-		$path = dirname(__FILE__);
 		$Suite->addTestDirectory($path . DS . 'View' . DS . 'Helper');
 
-		$path = dirname(__FILE__);
 		$Suite->addTestDirectory($path . DS . 'Model');
 
-		$path = dirname(__FILE__);
 		$Suite->addTestDirectory($path . DS . 'Model' . DS . 'Behavior');
 
 		return $Suite;
