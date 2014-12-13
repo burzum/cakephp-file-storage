5e5023125e6ee9ffa7117b01618e9d1b002a42c8
Florian Krämer <florian.kraemer@cakedc.com>
2014-12-13 03:04:57 +0100

    Working on the test setup

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
-use Cake\Event\EventManager;
-use Burzum\FileStorage\Event\ImageProcessingListener;
-use Burzum\FileStorage\Event\LocalFileStorageListener;
+use \Cake\Event\EventManager;
+use \Burzum\FileStorage\Event\ImageProcessingListener;
+use \Burzum\FileStorage\Event\LocalFileStorageListener;
 
 $listener = new ImageProcessingListener();
 EventManager::instance()->attach($listener);
