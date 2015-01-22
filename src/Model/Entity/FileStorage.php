095f1909c62810d121a3b390417bc57e4086f426
Florian Krämer <florian.kraemer@cakedc.com>
2015-01-22 01:48:45 +0100

    Adding the FileStorage entity class.

---
+++ src/Model/Entity/FileStorage.php
@@ -0,0 +1,32 @@
+<?php
+namespace Burzum\FileStorage\Model\Entity;
+
+use Cake\ORM\Entity;
+
+/**
+ * FileStorage Entity.
+ */
+class FileStorage extends Entity {
+
+/**
+ * Fields that can be mass assigned using newEntity() or patchEntity().
+ *
+ * @var array
+ */
+	protected $_accessible = [
+		'id' => true,
+		'user_id' => true,
+		'foreign_key' => true,
+		'model' => true,
+		'filename' => true,
+		'filesize' => true,
+		'mime_type' => true,
+		'extension' => true,
+		'hash' => true,
+		'path' => true,
+		'adapter' => true,
+		'created' => true,
+		'modified' => true,
+	];
+
+}
