98515f95d3e89d7c1dd1ad6fee0670700eff0f63
Robert <r.pustulka@zano.pl>
2015-08-03 15:06:44 +0200

    Revert default option.

--- src/Storage/PathBuilder/LocalPathBuilder.php
+++ src/Storage/PathBuilder/LocalPathBuilder.php
@@ -10,13 +10,4 @@ use Cake\ORM\Entity;
 
 class LocalPathBuilder extends BasePathBuilder {
 
-/**
- * Constructor
- *
- * @param array $config
- */
-	public function __construct(array $config = array()) {
-		$this->_defaultConfig['modelFolder'] = true;
-		parent::__construct($config);
-	}
 }
