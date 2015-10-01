52be9f1ad499544c63af433ef388012a2cc435d3
Florian Krämer <florian.kraemer@cakedc.com>
2015-10-02 01:46:27 +0200

    Fixing a bug in the BasePathBuilder.

--- src/Storage/PathBuilder/BasePathBuilder.php
+++ src/Storage/PathBuilder/BasePathBuilder.php
@@ -259,7 +259,7 @@ class BasePathBuilder implements PathBuilderInterface {
  * @return string
  */
 	public function url(EntityInterface $entity, array $options = []) {
-		$url = $this->path($entity) . $this->filename($entity);
+		$url = $this->path($entity) . $this->filename($entity, $options);
 		return str_replace('\\', '/', $url);
 	}
 
