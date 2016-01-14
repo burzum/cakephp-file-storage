8e08f636cbf448a7c024d8a0e5f7746dd20e5208
Florian Krämer <florian.kraemer@cakedc.com>
2016-01-14 22:33:39 +0100

    Improving the BasePathBuilderTest

--- tests/TestCase/Storage/PathBuilder/BasePathBuilderTest.php
+++ tests/TestCase/Storage/PathBuilder/BasePathBuilderTest.php
@@ -6,13 +6,23 @@ use Cake\Core\InstanceConfigTrait;
 use Cake\ORM\TableRegistry;
 use Cake\TestSuite\TestCase;
 
+class TestBasePathBuilder extends BasePathBuilder {
+
+	/**
+	 *
+	 */
+	public function randomPathTestMethod($string) {
+		return $string . 'test';
+	}
+}
+
 class BasePathBuilderTest extends TestCase {
 
-/**
- * Fixtures
- *
- * @var array
- */
+	/**
+	 * Fixtures
+	 *
+	 * @var array
+	 */
 	public $fixtures = array(
 		'plugin.Burzum\FileStorage.FileStorage'
 	);
@@ -36,11 +46,11 @@ class BasePathBuilderTest extends TestCase {
 		$this->entity->accessible('id', true);
 	}
 
-/**
- * testPathbuilding
- *
- * @return void
- */
+	/**
+	 * testPathbuilding
+	 *
+	 * @return void
+	 */
 	public function testPathbuilding() {
 		$builder = new BasePathBuilder();
 		$config = $builder->config();
@@ -73,30 +83,60 @@ class BasePathBuilderTest extends TestCase {
 		$result = $builder->path($this->entity);
 		$this->assertEquals($result, '14' . DS . '83' . DS . '23' . DS . 'filestorage1' . DS . 'files' . DS);
 
+		$builder->config($config);
+		$builder->config('pathPrefix', 'files');
+		$result = $builder->path($this->entity);
+		$this->assertEquals($result, 'files' . DS . '14' . DS . '83' . DS . '23' . DS . 'filestorage1' . DS);
+
+		$builder->config($config);
 		$result = $builder->url($this->entity);
-		$expected = '14/83/23/filestorage1/files/filestorage1.png';
+		$expected = '14/83/23/filestorage1/filestorage1.png';
 		$this->assertEquals($result, $expected);
 	}
 
-/**
- * testRandomPath
- *
- * @return void
- */
+	/**
+	 * testRandomPath
+	 *
+	 * @return void
+	 */
 	public function testRandomPath() {
-		$builder = new BasePathBuilder();
+		$builder = new TestBasePathBuilder();
 		$result = $builder->randomPath('test', 5, 'sha1');
 		$this->assertEquals($result, '4a' . DS . '8f' . DS . 'e5' . DS . 'cc' . DS . 'b1' . DS);
 
 		$result = $builder->randomPath('test', 3, 'sha1');
 		$this->assertEquals($result, '4a' . DS . '8f' . DS . 'e5' . DS);
+
+		if (PHP_INT_SIZE === 4) {
+			$result = $builder->randomPath('test', 3, 'crc32');
+			$this->assertEquals($result, '00' . DS . '33' . DS . '73' . DS);
+		}
+
+		if (PHP_INT_SIZE === 8) {
+			$result = $builder->randomPath('test', 3, 'crc32');
+			$this->assertEquals($result, '96' . DS . '39' . DS . '23' . DS);
+		}
+
+		$result = $builder->randomPath('test', 3, 'randomPathTestMethod');
+		$this->assertEquals($result, 'testtest');
 	}
 
-/**
- * testEnsureSlash
- *
- * @return void
- */
+	/**
+	 * testRandomPathInvalidArgumentException
+	 *
+	 * @expectedException \InvalidArgumentException
+	 * @return void
+	 */
+	public function testRandomPathInvalidArgumentException() {
+		$builder = new BasePathBuilder();
+		$result = $builder->randomPath('test', 5, 'does-not-exist');
+	}
+
+	/**
+	 * testEnsureSlash
+	 *
+	 * @return void
+	 */
 	public function testEnsureSlash() {
 		$string = 'foo/bar';
 		$builder = new BasePathBuilder();
@@ -107,22 +147,22 @@ class BasePathBuilderTest extends TestCase {
 		$this->assertEquals($result, DS . $string . DS);
 	}
 
-/**
- * testEnsureSlashInvalidArgumentException
- *
- * @expectedException \InvalidArgumentException
- */
+	/**
+	 * testEnsureSlashInvalidArgumentException
+	 *
+	 * @expectedException \InvalidArgumentException
+	 */
 	public function testEnsureSlashInvalidArgumentException() {
 		$string = 'foo/bar';
 		$builder = new BasePathBuilder();
 		$builder->ensureSlash($string, 'INVALID!');
 	}
 
-/**
- * testSplitFilename
- *
- * @return void
- */
+	/**
+	 * testSplitFilename
+	 *
+	 * @return void
+	 */
 	public function testSplitFilename() {
 		$builder = new BasePathBuilder();
 		$result = $builder->splitFilename('some.fancy.name.jpg');
@@ -140,11 +180,11 @@ class BasePathBuilderTest extends TestCase {
 		$this->assertEquals($result, $expected);
 	}
 
-/**
- * testStripDashes
- *
- * @return void
- */
+	/**
+	 * testStripDashes
+	 *
+	 * @return void
+	 */
 	public function testStripDashes() {
 		$builder = new BasePathBuilder();
 		$result = $builder->stripDashes('with-dashes-!');
