6a7a10e9178f46a22ad588e17d4a19e34538c2c1
Florian Krämer <florian.kraemer@cakedc.com>
2016-12-16 11:20:24 +0100

    Tests

---
+++ tests/TestCase/View/Helper/ImageHelperTest.php
@@ -0,0 +1,111 @@
+<?php
+namespace Burzum\FileStorage\Test\TestCase\View\Helper;
+
+use Burzum\FileStorage\Test\TestCase\FileStorageTestCase;
+use Burzum\FileStorage\View\Helper\ImageHelper;
+use Cake\Core\Configure;
+use Cake\Network\Request;
+use Cake\View\Helper\HtmlHelper;
+use Cake\View\View;
+
+/**
+ * ImageHelperTest
+ *
+ * @author Florian Krämer
+ * @copy 2012 - 2016 Florian Krämer
+ * @license MIT
+ */
+class ImageHelperTest extends FileStorageTestCase {
+
+	/**
+	 * Image Helper
+	 *
+	 * @var \Burzum\FileStorage\View\Helper\ImageHelper|null
+	 */
+	public $Image = null;
+
+	/**
+	 * Image Helper
+	 *
+	 * @var \Cake\View\View|null
+	 */
+	public $View = null;
+
+	/**
+	 * Start Test
+	 *
+	 * @return void
+	 */
+	public function setUp() {
+		parent::setUp();
+		$null = null;
+		$this->View = new View($null);
+		$this->Image = new ImageHelper($this->View);
+		$this->Image->Html = new HtmlHelper($this->View);
+		$this->Image->Html->request = new Request('contacts/add', false);
+		$this->Image->Html->request->webroot = '/';
+		$this->Image->Html->request->base = '/';
+	}
+
+	/**
+	 * End Test
+	 *
+	 * @return void
+	 */
+	public function tearDown() {
+		parent::tearDown();
+		unset($this->Image);
+	}
+
+	public function testImageUrl() {
+		$image = $this->FileStorage->newEntity([
+			'id' => 'e479b480-f60b-11e1-a21f-0800200c9a66',
+			'filename' => 'testimage.jpg',
+			'model' => 'Test',
+			'path' => 'test/path/testimage.jpg',
+			'extension' => 'jpg',
+			'adapter' => 'Local'
+		], ['accessibleFields' => ['*' => true]]);
+
+		$result = $this->Image->imageUrl($image, 't150', ['pathPrefix' => '/src/']);
+		debug($result);
+	}
+
+	/**
+	 * testImage
+	 *
+	 * @expectedException \InvalidArgumentException
+	 * @return void
+	 */
+	public function testImageUrlInvalidArgumentException() {
+		$image = $this->FileStorage->newEntity([
+			'id' => 'e479b480-f60b-11e1-a21f-0800200c9a66',
+			'filename' => 'testimage.jpg',
+			'model' => 'Test',
+			'path' => 'test/path/',
+			'extension' => 'jpg',
+			'adapter' => 'Local'
+		], ['accessibleFields' => ['*' => true]]);
+
+		$this->Image->imageUrl($image, 'invalid-version!');
+	}
+
+	/**
+	 * testFallbackImage
+	 *
+	 * @return void
+	 */
+	public function testFallbackImage() {
+		Configure::write('Media.fallbackImages.Test.t150', 't150fallback.png');
+
+		$result = $this->Image->fallbackImage(['fallback' => true], [], 't150');
+		$this->assertEquals($result, '<img src="/img/placeholder/t150.jpg" alt=""/>');
+
+		$result = $this->Image->fallbackImage(['fallback' => 'something.png'], [], 't150');
+		$this->assertEquals($result, '<img src="/img/something.png" alt=""/>');
+
+		$result = $this->Image->fallbackImage([], [], 't150');
+		$this->assertEquals($result, '');
+	}
+
+}
