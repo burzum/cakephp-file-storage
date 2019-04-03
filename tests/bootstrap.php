3f53ed731f9e456618673d5861c0d31c29072e0d
Marc Würth <ravage@bluewin.ch>
2019-04-03 18:53:14 +0200

    Load Plugin for CakePHP greater or equal 3.7.0
    Solution found at: https://github.com/cakephp/cakephp/issues/12682

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -27,8 +27,12 @@ $loader->setPsr4('Cake\\', './vendor/cakephp/cakephp/src');
 $loader->setPsr4('Cake\Test\\', './vendor/cakephp/cakephp/tests');
 $loader->setPsr4('Burzum\Imagine\\', './vendor/burzum/cakephp-imagine-plugin/src');
 
+$config = [
+	'path' => dirname(__FILE__, 2) . DS,
+];
 if (\version_compare(\Cake\Core\Configure::version(), '3.7.0', 'lt')) {
-	Plugin::load('Burzum/FileStorage', [
-		'path' => dirname(__FILE__, 2) . DS,
-	]);
+	Plugin::load('Burzum/FileStorage', $config);
+} else {
+	$plugin = new \Burzum\FileStorage\Plugin($config);
+	Plugin::getCollection()->add($plugin);
 }
